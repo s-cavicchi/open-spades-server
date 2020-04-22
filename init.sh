e9b145a4426dfad2e9d475c36cfa0c3880dc0146
Samuel-Cavicchi <samuel@webtile.com.au>
2020-04-22 19:25:16 +1000

    Add startup scripts

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+S3URI=s3://open-spades
+GIT_REPO=https://github.com/Samuel-Cavicchi/open-spades-server.git
+
+git clone $GIT_REPO || true
+
+# Just in case we want to keep data
+git stash
+git fetch
+git reset --hard origin/master
+
+aws s3 cp $S3URI/passwords.toml ./passwords.toml
+cat passwords.toml >> config.toml
+
+sh start.sh
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CONTAINER_NAME=cheeky
+docker run -d \
+           --rm \
+           -v $PWD:/config \
+           -p 32887:32887/udp -p 32886:32886/tcp \
+           --name $CONTAINER_NAME \
+           piqueserver/piqueserver:master
+
