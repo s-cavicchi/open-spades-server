d35b83e461fb1e3cbcd572f48d3aebb822feeaa6
Samuel-Cavicchi <samuel@webtile.com.au>
2020-04-29 00:00:11 +1000

    Change basedir to use cd

--- init.sh
+++ init.sh
@@ -4,12 +4,14 @@ S3URI=s3://open-spades
 GIT_REPO=https://github.com/Samuel-Cavicchi/open-spades-server.git
 BASEDIR=$(dirname "$0")
 
+cd $BASEDIR
+
 # Just in case we want to keep data
 git stash
 git fetch
 git reset --hard origin/master
 
-aws s3 cp $S3URI/passwords.toml $BASEDIR/passwords.toml
-cat $BASEDIR/passwords.toml >> $BASEDIR/config.toml
+aws s3 cp $S3URI/passwords.toml ./passwords.toml
+cat ./passwords.toml >> ./config.toml
 
-sh $BASEDIR/start.sh
+sh ./start.sh
