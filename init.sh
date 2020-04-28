b79d3ce6adfe927aff31f6675a91735bb7b900b6
Samuel-Cavicchi <samuel@webtile.com.au>
2020-04-28 23:57:35 +1000

    Add basedir to init script

--- init.sh
+++ init.sh
@@ -2,13 +2,14 @@
 
 S3URI=s3://open-spades
 GIT_REPO=https://github.com/Samuel-Cavicchi/open-spades-server.git
+BASEDIR=$(dirname "$0")
 
 # Just in case we want to keep data
 git stash
 git fetch
 git reset --hard origin/master
 
-aws s3 cp $S3URI/passwords.toml ./passwords.toml
-cat passwords.toml >> config.toml
+aws s3 cp $S3URI/passwords.toml $BASEDIR/passwords.toml
+cat $BASEDIR/passwords.toml >> $BASEDIR/config.toml
 
-sh ./start.sh
+sh $BASEDIR/start.sh
